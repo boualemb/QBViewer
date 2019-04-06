49c89252e119cd9b536c27ecfb3e117cf6db5781
Bilal BOUALEM <bilal.boualem1@gmail.com>
2019-04-06 14:44:42 +0100

    experiments

---
+++ js/_viewer.js
@@ -0,0 +1,68 @@
+var camera, scene, renderer;
+var loader, geometry, material, mesh;
+
+var gltflink = 'models/gltf/flag/Flag_Algeria.glb';
+gltflink = 'https://raw.githubusercontent.com/boualemb/glTF-Viewer/master/models/gltf/flag/Flag_Algeria.glb';
+
+init();
+animate();
+ 
+function init() {
+    //const canvas = document.querySelector('#canvas');
+    camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 10 );
+    camera.position.z = 2;
+    
+    scene = new THREE.Scene();
+
+    var light = new THREE.HemisphereLight( 0xffffbb, 0x080820, 1 );
+    scene.add( light );
+    loader = new THREE.GLTFLoader();
+    loader.load(gltflink, 
+    function(gltf)
+    {
+        
+        scene.add(gltf.scene);
+        gltf.animations;
+        gltf.scene;
+        gltf.scenes;
+        gltf.cameras;
+        gltf.asset;
+
+    },
+    //called while loading
+    function(xhr)
+    {
+        if (xhr.total>0)
+        console.log( ((xhr.loaded/xhr.total)*100)  + ' % loaded' );
+    },
+    // called when loading has errors
+	function ( error ) {
+
+		console.log( 'An error happened' );
+
+	}
+
+    );
+    /*geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
+    material = new THREE.MeshNormalMaterial();
+ 
+    mesh = new THREE.Mesh( geometry, material );
+    scene.add( mesh );*/
+    
+    renderer = new THREE.WebGLRenderer( { antialias: true } );
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    renderer.setClearColor(0xEEEEEE);
+    document.body.appendChild( renderer.domElement );
+ 
+}
+ 
+function animate() {
+ 
+    requestAnimationFrame( animate );
+   /* if (mesh!=null)
+    mesh.rotation.y += 0.01;*/
+    //gltf.rotation.y += 0.02;
+ 
+    renderer.render( scene, camera );
+ 
+}
