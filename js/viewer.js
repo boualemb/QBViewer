ea40f177b9f8123bcd8fb37b60cc318179f0274f
Bilal BOUALEM <bilal.boualem1@gmail.com>
2019-04-06 11:40:34 +0100

    StartCommit
    NotWorking

---
+++ js/viewer.js
@@ -0,0 +1,65 @@
+var camera, scene, renderer;
+var loader, geometry, material, mesh;
+ 
+init();
+animate();
+ 
+function init() {
+    const canvas = document.querySelector('#canvas');
+    camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 10 );
+    camera.position.z = 50;
+    
+    scene = new THREE.Scene();
+
+    var light = new THREE.AmbientLight( 0x404040 ); // soft white light
+    scene.add( light );
+    loader = new THREE.GLTFLoader();
+    loader.load('models/gltf/logo/QB_LOGO.glb', 
+    function(gltf)
+    {
+        
+        scene.add(gltf.scene);
+        gltf.animations;
+        gltf.scene;
+        gltf.scenes;
+        gltf.cameras;
+        gltf.asset;
+
+    },
+    //called while loading
+    function(xhr)
+    {
+        console.log(xhr.total);
+        if (xhr.total>0)
+        console.log( ((xhr.loaded/xhr.total)*100)  + ' % loaded' );
+    },
+    // called when loading has errors
+	function ( error ) {
+
+		console.log( 'An error happened' );
+
+	}
+
+    );
+    /*geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
+    material = new THREE.MeshNormalMaterial();
+ 
+    mesh = new THREE.Mesh( geometry, material );
+    scene.add( mesh );*/
+    
+    renderer = new THREE.WebGLRenderer( { antialias: true } );
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    document.body.appendChild( renderer.domElement );
+ 
+}
+ 
+function animate() {
+ 
+    requestAnimationFrame( animate );
+    if (mesh!=null)
+    mesh.rotation.y += 0.01;
+    //gltf.rotation.y += 0.02;
+ 
+    renderer.render( scene, camera );
+ 
+}
